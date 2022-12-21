bb4174cc97a4c053c0ae65c457b4419b2096ad10
Saahil Patel <saahpate456@ed.amdsb.ca>
2022-12-21 11:27:25 -0500

    Add project files.

---
+++ spaceRace/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace spaceRace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameLoop = new System.Windows.Forms.Timer(this.components);
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.p1Score = new System.Windows.Forms.Label();
+            this.p2Score = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameLoop
+            // 
+            this.gameLoop.Interval = 20;
+            this.gameLoop.Tick += new System.EventHandler(this.gameLoop_Tick);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.timeLabel.Location = new System.Drawing.Point(350, 9);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(118, 24);
+            this.timeLabel.TabIndex = 0;
+            this.timeLabel.Text = "timeLabel";
+            // 
+            // p1Score
+            // 
+            this.p1Score.AutoSize = true;
+            this.p1Score.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p1Score.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.p1Score.Location = new System.Drawing.Point(365, 43);
+            this.p1Score.Name = "p1Score";
+            this.p1Score.Size = new System.Drawing.Size(34, 24);
+            this.p1Score.TabIndex = 1;
+            this.p1Score.Text = "P1";
+            // 
+            // p2Score
+            // 
+            this.p2Score.AutoSize = true;
+            this.p2Score.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p2Score.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.p2Score.Location = new System.Drawing.Point(423, 43);
+            this.p2Score.Name = "p2Score";
+            this.p2Score.Size = new System.Drawing.Size(34, 24);
+            this.p2Score.TabIndex = 2;
+            this.p2Score.Text = "P2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.p2Score);
+            this.Controls.Add(this.p1Score);
+            this.Controls.Add(this.timeLabel);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameLoop;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label p1Score;
+        private System.Windows.Forms.Label p2Score;
+    }
+}
+
---
+++ spaceRace/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace spaceRace
+{
+    public partial class Form1 : Form
+    {
+        Rectangle player1 = new Rectangle(650, 575, 20, 20);
+        Rectangle player2 = new Rectangle(150, 575, 20, 20);
+        int playerSpeed = 7;
+
+
+        Rectangle safezone = new Rectangle();
+        int safezoneHeight = 50;
+
+        //asteroids
+        List<Rectangle> asteroids = new List<Rectangle>();
+        List<int> asteroidSpeeds = new List<int>();
+
+        int asteroidSize = 15;
+
+        //random
+        Random randGen = new Random();
+        int randValue = 0;
+
+        //player movement
+        bool wDown = false;
+        bool sDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+
+        //colours
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+        SolidBrush grayBrush = new SolidBrush (Color.Gray);
+
+        public Form1()
+        {
+            InitializeComponent();
+            //safezone = new Rectangle(0, this.Height - safezoneHeight, this.Width, safezoneHeight);
+            gameLoop.Enabled = true;
+        }
+
+        private void gameLoop_Tick(object sender, EventArgs e)
+        {
+            //move player 1 
+            if (wDown == true && player1.Y > 0)
+            {
+                player1.Y -= playerSpeed;
+            }
+            if (sDown == true && player1.Y < this.Height - player1.Height)
+            {
+                player1.Y += playerSpeed;
+            }
+
+            //move player 2 
+            if (upArrowDown == true && player2.Y > 0)
+            {
+                player2.Y -= playerSpeed;
+            }
+            if (downArrowDown == true && player2.Y < this.Height - player2.Height)
+            {
+                player2.Y += playerSpeed;
+            }
+
+            //move ball objects
+            for (int i = 0; i < asteroids.Count; i++)
+            {
+                int y = asteroids[i].X + asteroidSpeeds[i];
+                asteroids[i] = new Rectangle(asteroids[i].Y, y, asteroidSize, asteroidSize);
+            }
+            //generate a random value
+            randValue = randGen.Next(1, 101);
+
+            //generate new ball if it is time
+            if (randValue < 3)
+            {
+                asteroids.Add(new Rectangle(randGen.Next(0, this.Width - asteroidSize), 0, asteroidSize, asteroidSize));
+                asteroidSpeeds.Add(4);
+            }
+            else if (randValue < 8)
+            {
+                asteroids.Add(new Rectangle(randGen.Next(0, this.Width - asteroidSize), 0, asteroidSize, asteroidSize));
+                asteroidSpeeds.Add(8);
+            }
+            else if (randValue < 20)
+            {
+                asteroids.Add(new Rectangle(randGen.Next(0, this.Width - asteroidSize), 0, asteroidSize, asteroidSize));
+                asteroidSpeeds.Add(12);
+            }
+
+            Refresh();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //draw players
+            e.Graphics.FillRectangle(redBrush, player1);
+            e.Graphics.FillRectangle(yellowBrush, player2);
+
+            //draw asteroids
+            for (int i = 0; i < asteroids.Count; i++)
+            {
+                e.Graphics.FillRectangle(grayBrush, asteroids[i]);
+            }
+
+
+            //////draw safezone
+            //e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(155, 155, 155, 155)), safezone);
+
+            //////draw safezone
+            ////e.Graphics.FillRectangle(grayBrush, 0, this.Height - safezoneHeight,
+            ////    this.Width, safezoneHeight);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
